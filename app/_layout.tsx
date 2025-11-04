5e62ed6d98fdc17ec4596355a4e06b41ed04fa70
Oszust Kacper <oszust.kacper@gmail.com>
2025-11-04 11:26:47 +0100

    oui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,34 @@ import "react-native-reanimated";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import PostListScreen from "./screens/PostListScreen";
 import PostDetailScreen from "./screens/PostDetailScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import HomeScreen from "./screens/home";
+import ProfileScreen from "./screens/ProfileScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export type RootStackParamList = {
   PostList: undefined;
   PostDetail: { postId: string; title: string; content: string };
+  HomeScreen: undefined;
+  ProfileScreen: undefined;
+  SettingsScreen: undefined; 
 };
 
 export default function RootLayout() {
   const Stack = createNativeStackNavigator<RootStackParamList>();
 
-  return <>{/* Your code here for the stack navigator */}</>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="HomeScreen">
+        <Stack.Screen name="PostList" component={PostListScreen} />
+        <Stack.Screen name="PostDetail" component={PostDetailScreen} />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="SettingsScreen" component={SettingsScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+
